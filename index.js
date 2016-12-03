dffd6ebbdf74cf6b2d5697016a16e26a9d8ae644
Alexandre Bintz <alexandre.bintz@gmail.com>
2016-12-03 11:59:26 +0100

    Refactor check code

--- index.js
+++ index.js
@@ -19,32 +19,51 @@ let appName = config.appName
 let packageName = config.packageName
 let timeInterval = duration.parse(config.timeInterval)
 
-let url = 'https://play.google.com/store/apps/details?id=' + packageName
-
 let mailServer = email.server.connect(config.smtp)
 
 function check () {
   console.log((new Date()) + ' 🔮  Checking if ' + appName + ' (' + packageName + ') is up on the PlayStore...')
-  http.request(url, (res) => {
-    if (res.statusCode === 200) {
-      growl('🎉  ' + appName + ' is up on the PlayStore !', {open: url})
-      mailServer.send({
-        to: config.emailTo,
-        from: config.emailFrom,
-        subject: '🎉  ' + appName + ' is up on the PlayStore !',
-        text: '🎉  ' + appName + ' is up on the PlayStore !'
-      }, (err, message) => {
-        if (err) {
-          console.log(err)
-        }
-      })
-      clearInterval(interval)
+  checkPlayStore(packageName, (err, isUp) => {
+    if (!err) {
+      if (isUp) {
+        growl('🎉  ' + appName + ' is up on the PlayStore !')
+        mailServer.send({
+          to: config.emailTo,
+          from: config.emailFrom,
+          subject: '🎉  ' + appName + ' is up on the PlayStore !',
+          text: '🎉  ' + appName + ' is up on the PlayStore !'
+        }, (err, message) => {
+          if (err) {
+            console.log(err)
+          }
+        })
+        clearInterval(interval)
+      }
     }
-  }).on('error', (e) => {
-    console.log(`problem with request: ${e.message}`)
-  }).end()
+  })
 }
 
 let interval = setInterval(check, timeInterval.milliseconds())
 
 check()
+
+function checkPlayStore (packageName, cb) {
+  let url = getPlayStoreUrl(packageName)
+  http.request(url, (res) => {
+    let isUp = getAndroidAppIsUp(res)
+    cb(null, isUp)
+  }).on('error', (err) => {
+    console.error(err)
+    cb(err)
+  }).end()
+}
+
+function getPlayStoreUrl (packageName) {
+  let url = 'https://play.google.com/store/apps/details?id=' + packageName
+  return url
+}
+
+function getAndroidAppIsUp (httpResponse) {
+  let isUp = httpResponse.statusCode === 200
+  return isUp
+}
