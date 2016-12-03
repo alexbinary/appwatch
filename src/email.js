1341e7562c7282956a25dec213c1c928a9c2bfbb
Alexandre Bintz <alexandre@bintz.io>
2016-12-03 16:56:24 +0100

    Multiple email recipients

--- src/email.js
+++ src/email.js
@@ -1,12 +1,21 @@
 
 var email = require('emailjs')
 
+function joinAddresses (addresses) {
+  let joined = ''
+  for (let i in addresses) {
+    if (joined) joined += ', '
+    joined += addresses[i]
+  }
+  return joined
+}
+
 function createAgent (config) {
   let mailServer = email.server.connect(config.smtp)
   return {
     send: function (appName, packageName) {
       mailServer.send({
-        to: config.to,
+        to: joinAddresses(config.to),
         from: config.from,
         subject: '🎉  ' + appName + ' is up on the PlayStore !',
         text: '🎉  ' + appName + ' is up on the PlayStore !'
