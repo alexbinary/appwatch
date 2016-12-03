6e9d1616d4fbaa2b15f354cdba19a8b9d948bf04
Alexandre Bintz <alexandre@bintz.io>
2016-12-03 15:57:56 +0100

    Run forever

--- src/index.js
+++ src/index.js
@@ -6,30 +6,23 @@ var email = require('./email')
 let config = require('./config')
 let playstore = require('./playstore')
 
-let conf = config.get()
-
-let appName = conf.appName
-let packageName = conf.packageName
-let timeInterval = duration.parse(conf.timeInterval)
-
-let mailAgent = email({
-  smtp: conf.smtp,
-  from: conf.emailFrom,
-  to: conf.emailTo
-})
-
-function check () {
+;(function check () {
+  let conf = config.get()
+  let appName = conf.appName
+  let packageName = conf.packageName
+  let timeInterval = duration.parse(conf.timeInterval)
+  let mailAgent = email({
+    smtp: conf.smtp,
+    from: conf.emailFrom,
+    to: conf.emailTo
+  })
   console.log((new Date()) + ' 🔮  Checking if ' + appName + ' (' + packageName + ') is up on the PlayStore...')
   playstore.check(packageName, (err, isUp) => {
     if (!err) {
       if (isUp) {
         mailAgent.send(appName, packageName)
-        clearInterval(interval)
       }
     }
   })
-}
-
-let interval = setInterval(check, timeInterval.milliseconds())
-
-check()
+  setTimeout(check, timeInterval.milliseconds())
+})()
